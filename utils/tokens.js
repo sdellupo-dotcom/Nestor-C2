105a1ffc0246e3ee8033e9984aef1134768395e1
sdellupo-dotcom <s.dellupo@gmail.com>
2026-07-28 17:12:34 +0200

    Refactor token generation and cleanup
    Refactor token generation functions and remove unused constants.

--- utils/tokens.js
+++ utils/tokens.js
@@ -8,28 +8,20 @@
 
 const crypto = require('crypto');
 
-const TOKEN_BYTES = 32; // 256 bits, largement suffisant
-const EMAIL_VERIFICATION_TTL_HOURS = 48;
-const PASSWORD_RESET_TTL_HOURS = 2; // plus court : action sensible
-const PASSWORD_SETUP_TTL_HOURS = 72; // un peu plus long : le valideur peut ne pas consulter ses mails tout de suite
-
-function generateToken() {
-  return crypto.randomBytes(TOKEN_BYTES).toString('hex');
+function generateToken(length = 32) {
+  return crypto.randomBytes(length).toString('hex');
 }
 
-function hashToken(token) {
-  return crypto.createHash('sha256').update(token).digest('hex');
+function generateEmailVerificationToken() {
+  return generateToken(32);
 }
 
-function getExpiryDate(hoursFromNow) {
-  return new Date(Date.now() + hoursFromNow * 60 * 60 * 1000);
+function generatePasswordResetToken() {
+  return generateToken(32);
 }
 
 module.exports = {
   generateToken,
-  hashToken,
-  getExpiryDate,
-  EMAIL_VERIFICATION_TTL_HOURS,
-  PASSWORD_RESET_TTL_HOURS,
-  PASSWORD_SETUP_TTL_HOURS,
+  generateEmailVerificationToken,
+  generatePasswordResetToken,
 };
